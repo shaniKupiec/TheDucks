6bd4afdc9fd91c3d5614b3878f1be4c7954fa84f
shani <shani@webiks.com>
2023-08-03 17:06:46 +0300

    delete unused imports

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,5 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
-import type { RootState, AppDispatch } from './index'
+import type { RootState } from './index'
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
